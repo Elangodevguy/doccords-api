53f0e5172c97d435729240c46952faf9306292ca
Elango <elango@Elangos-MacBook-Pro.local>
2021-06-27 16:35:20 +0530

    demo changes

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const userController = require('./../controllers/userController')
+const { uploader } = require('../multerSetup')
+const imageUploader = require('../util/imageUploader')
+
+const router = express.Router()
+
+router.route('/').get(userController.getUserDetail).post(userController.createUser)
+
+router.route('/profiles').post(userController.createProfile)
+
+router.route('/upload/file').post(uploader.single('image'), imageUploader)
+module.exports = router
